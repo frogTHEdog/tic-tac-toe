3cd50a48eaeb881f272a62f6f278860e7cc4abe7
Maryana Konyukov <maryana.konyukova@gmail.com>
2020-03-21 15:06:49 +0300

    start

--- tictactoe.py
+++ tictactoe.py
@@ -3,20 +3,21 @@
 from tkinter import *
 
 root = Tk()
+root.geometry("300x300")
+root.title("Tic Tac Toe")
+
+btns = list()
+
+for i in range(9) :
+    btn = Button(text = "", background="#555555", foreground="#ffffff", width=10, height=10)
+    xx = (i % 3)*100
+    yy = int(i/3)*100 
+    btn.place(x = xx, y = yy ) 
+
+    btns.append(btn)
  
-e = Entry(width=20)
-b = Button(text="Преобразовать")
-l = Label(bg='black', fg='white', width=20)
- 
-def strToSortlist(event):
-    s = e.get()
-    s = s.split()
-    s.sort()
-    l['text'] = ' '.join(s)
- 
-b.bind('<Button-1>', strToSortlist)
- 
-e.pack()
-b.pack()
-l.pack()
+for btn in btns :
+    #btn.pack()
+    pass
+
 root.mainloop()
